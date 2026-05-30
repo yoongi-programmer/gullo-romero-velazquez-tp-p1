020864ae442b91e03bb60186d406cc82578b034b
melrom <melanieromero7111@gmail.com>
2026-05-30 11:06:02 -0300

    mapa y islas del piso dibujadas

---
+++ src/juego/Isla.java
@@ -0,0 +1,19 @@
+package juego;
+
+import java.awt.Color;
+import java.awt.Rectangle;
+
+import entorno.Entorno;
+
+public class Isla {
+	private Rectangle area;
+	
+	Isla(int x, int y, int ancho, int alto) {
+		this.area = new Rectangle(x,y,ancho,alto);
+	}
+	
+	public void dibujar(Entorno e) {
+        // Pide: x, y, ancho, alto, ángulo, color
+        e.dibujarRectangulo(this.area.x, this.area.y, this.area.width, this.area.height, 0, Color.green);
+    }
+}
--- src/juego/Juego.java
+++ src/juego/Juego.java
@@ -1,48 +1,63 @@
 package juego;
-
-
-import java.awt.Color;
-
+//import java.awt.Color;
+import java.awt.Image;
 import entorno.Entorno;
+import entorno.Herramientas;
 import entorno.InterfaceJuego;
 
-public class Juego extends InterfaceJuego
-{
+public class Juego extends InterfaceJuego {
 	// El objeto Entorno que controla el tiempo y otros
 	private Entorno entorno;
-	
-	// Variables y métodos propios de cada grupo
-	// ...
-	
-	Juego()
-	{
+	private double x;
+	private double y;
+	private Image imagen; 
+    private Isla[] islas;
+    
+	Juego(double x, double y) {
 		// Inicializa el objeto entorno
-		this.entorno = new Entorno(this, "Proyecto para TP", 800, 600);
+		this.entorno = new Entorno(this, "Proyecto para TP", 800, 500);
+		this.x = x;
+		this.y = y;
+		this.islas = new Isla[30];
+		inicializarPiso();
 		
-		// Inicializar lo que haga falta para el juego
-		// ...
-
+        // Cargamos la imagen UNA SOLA VEZ al nacer el objeto.
+        this.imagen = Herramientas.cargarImagen("img/fondo3.png");
 		// Inicia el juego!
 		this.entorno.iniciar();
 	}
+	// Comportamiento
+    public void dibujar(Entorno e) {
+        // Ángulo 0 significa sin rotación. Escala 1.0 es el tamaño original.
+        e.dibujarImagen(this.imagen, this.x, this.y, 0, 2); //(imagen, coordenada X, coordenada Y, ángulo, escala)
+    }
+    
+    private void inicializarPiso() {
+		// La primera isla arranca en X=100, Y=550 (bien abajo)
+		int posX = 50;
+		int anchoIsla = 100;
+		int separacion = 50; // El hueco para que la princesa caiga
 
-	/**
-	 * Durante el juego, el método tick() será ejecutado en cada instante y 
-	 * por lo tanto es el método más importante de esta clase. Aquí se debe 
-	 * actualizar el estado interno del juego para simular el paso del tiempo 
-	 * (ver el enunciado del TP para mayor detalle).
-	 */
-	public void tick()
-	{
+		for (int i = 0; i < 10; i++) {
+			this.islas[i] = new Isla(posX, 490, anchoIsla, 50);
+			posX = posX + anchoIsla + separacion; // sumando el ancho de la isla que acabamos de crear + el hueco 
+		}
+	}
+	public void tick() {
 		// Procesamiento de un instante de tiempo
-		// ...
+		this.dibujar(this.entorno);
+		//dibujar islas
+		for(int i = 0; i < this.islas.length; i++) {
+			if(this.islas[i] != null) {
+				this.islas[i].dibujar(this.entorno);
+			}
+		}
 		
 	}
 	
-
 	@SuppressWarnings("unused")
-	public static void main(String[] args)
-	{
-		Juego juego = new Juego();
+	public static void main(String[] args) {
+		@SuppressWarnings("unused")
+		Juego juego = new Juego(800,250);
 	}
 }
