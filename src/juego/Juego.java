a77738efae306d27779ad750eff1cf1c805dc245
melrom <melanieromero7111@gmail.com>
2026-05-31 18:28:31 -0300

    style: clase de java ordenada y comentada

--- src/juego/Juego.java
+++ src/juego/Juego.java
@@ -1,12 +1,10 @@
 package juego;
-//import java.awt.Color;
 import java.awt.Image;
 import entorno.Entorno;
 import entorno.Herramientas;
 import entorno.InterfaceJuego;
 
 public class Juego extends InterfaceJuego {
-	// El objeto Entorno que controla el tiempo y otros
 	private Entorno entorno;
 	private double xMapa;
 	private double yMapa;
@@ -15,31 +13,25 @@ public class Juego extends InterfaceJuego {
     private Princesa princesa;
 	private Enemigos[] enemigo;
 	private Disparo disparo;
-    
+	
+	//CONSTRUCTOR DEL JUEGO------------------------------------------------------------------------------
 	Juego(double x, double y) {
-		// Inicializa el objeto entorno
 		this.entorno = new Entorno(this, "Proyecto para TP", 1000, 500);
 		this.xMapa = x;
 		this.yMapa = y;
 		this.islas = new Isla[30];
-		inicializarPiso();
-		
-        // Cargamos la imagen UNA SOLA VEZ al nacer el objeto.
-        this.imagen = Herramientas.cargarImagen("img/fondo3.png");
-		// Inicia el juego!
-		this.entorno.iniciar();
-		
-		//iNSTANCIA DE PRINCESA
-		this.princesa = new Princesa (400 , 200, 25, 35,2.5); // (coordenada X, coordenada Y, ancho, alto, velocidad)
 		this.enemigo = new Enemigos[10];
+		this.princesa = new Princesa (400 , 200, 25, 35,2.5); // (coord X, coord Y, ancho, alto, velocidad)
+		
+        this.imagen = Herramientas.cargarImagen("img/fondo3.png");
+		this.entorno.iniciar(); // Inicia el juego!
+		inicializarPiso();
 	}
-	
-	// Comportamiento
+	//METODOS DE COMPORTAMIENTO---------------------------------------------------------------------------
     public void dibujar(Entorno e) {
-        // Ángulo 0 significa sin rotación. Escala 1.0 es el tamaño original.
         e.dibujarImagen(this.imagen, this.xMapa, this.yMapa, 0, 2); //(imagen, coordenada X, coordenada Y, ángulo, escala)
     }
-    
+    //--------------------------------------------------------------------- Mapa
     private void inicializarPiso() {
 		// La primera isla arranca en X=100, Y=550 (bien abajo)
 		int posX = 100;
@@ -52,18 +44,17 @@ public class Juego extends InterfaceJuego {
 		} 		
 		
 	}
-
     public void moverMapaIzquierda() {
         for (int i = 0; i < this.islas.length; i++) {
             if (this.islas[i] != null) {
-                this.islas[i].moverIzquierda(); // A la Isla también hay que hacerle un método que haga: this.x -= 3
+                this.islas[i].moverIzquierda();
             }
         }
         this.xMapa-=3; //Muevo el fondo tambien
     }
-    
+    //--------------------------------------------------------------------- Estado interno del juego
 	public void tick() {
-		// Control del movimiento de la Princesa-----------------------------
+		// Control del movimiento de la Princesa--------------------------------
 		if (this.entorno.estaPresionada(this.entorno.TECLA_DERECHA)) {
 			// Si la princesa aún no llegó a la mitad, avanza ella
 	        if (this.princesa.getX() < 500) {
@@ -87,24 +78,14 @@ public class Juego extends InterfaceJuego {
 		if (entorno.estaPresionada(entorno.TECLA_ABAJO)) {
 			princesa.moverseAbajo();
 		}
-		//Dibujar fondo----------------------------------------------
-		this.dibujar(this.entorno);
-		//dibujar islas----------------------------------------------
-		for(int i = 0; i < this.islas.length; i++) {
-			if(this.islas[i] != null) {
-				this.islas[i].dibujar(this.entorno);
-			}
-		}
-		//Dibujar princesa-------------------------------------------
-		princesa.dibujarse(this.entorno);
-		// Crear disparo
+		
+		// ENEMIGOS Y DISPAROS---------------------------------------------------
 		if (entorno.sePresiono(entorno.TECLA_ESPACIO) && disparo == null) {
 			disparo = new Disparo(
 					princesa.getX(),
 					princesa.getY(),
 					princesa.estaMirandoDerecha());
 		}
-
 		// Mover disparo
 		if (disparo != null) {
 			disparo.mover();
@@ -114,7 +95,6 @@ public class Juego extends InterfaceJuego {
 				disparo = null;
 			}
 		}
-
 		// Mantener mínimo 3 enemigos
 		int vivos = 0;
 		for (int i = 0; i < enemigo.length; i++) {
@@ -132,13 +112,22 @@ public class Juego extends InterfaceJuego {
 				}
 			}
 		}
-
-		// Dibujar enemigos
+		
+		//DIBUJAR TODO-----------------------------------------------------------
+		this.dibujar(this.entorno);		//fondo
+		//----------------------------------------------- Dibujar islas
+		for(int i = 0; i < this.islas.length; i++) {
+			if(this.islas[i] != null) {
+				this.islas[i].dibujar(this.entorno);
+			}
+		}
+		//----------------------------------------------- Dibujar princesa
+		princesa.dibujarse(this.entorno);
+		//----------------------------------------------- Dibujar enemigos
 		for (int i = 0; i < enemigo.length; i++) {
 			if (enemigo[i] != null) {
 				enemigo[i].mover();
 				enemigo[i].dibujar(entorno);
-
 				// Colisión disparo-enemigo
 				if (disparo != null &&
 					enemigo[i].colisionDisparoEnemigo(disparo)) {
@@ -147,16 +136,14 @@ public class Juego extends InterfaceJuego {
 					disparo = null;
 					continue;
 				}
-
 				// Sale de pantalla
 				if (enemigo[i].fueraDePantalla()) {
 					enemigo[i] = null;
 				}
 			}
 		}
-	
 	}
-	//-------------------------------------------------MAIN
+	//-----------------------------------------------------------------------MAIN
 	@SuppressWarnings("unused")
 	public static void main(String[] args) {
 		@SuppressWarnings("unused")
